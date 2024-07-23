b8888a25db32c647aa53b3bbb1a33c75dfe23add
Abhishek-kumar41 <abhishek.akumar411@gmail.com>
2024-07-23 16:21:17 +0530

    updates: updating and removing records based on ID

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch, Delete, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -11,4 +12,26 @@ export class UsersController {
     signup(@Body() body: CreateUserDto) {
         this.userService.create(body.email, body.password);
     }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string){
+        return this.userService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUser(@Query('email') email: string) {
+        return this.userService.find(email);
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.userService.update(parseInt(id), body);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id));
+    }
+
+    
 }
