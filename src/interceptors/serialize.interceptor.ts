48246d10e43d6df1a5425e324ec00cee8877a7b2
Abhishek-kumar41 <abhishek.akumar411@gmail.com>
2024-07-24 15:48:14 +0530

    updates: custom types for interceptor, custom decorator, changes in controller, controller wide serialization and route handler based serialization

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -3,15 +3,31 @@ import { Observable } from "rxjs";
 import { map } from "rxjs/operators"
 import { plainToClass } from "class-transformer";
 
-export class SerializeInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-        // Run something before a request is handled by the request handler
-        console.log('Running before the handler', context);
 
+interface ClassConstructor {
+    new (...args: any[]): {}
+}
+//* Decorator (custom)
+export function Serialize(dto: ClassConstructor) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+
+    constructor(private dto: any) {}
+    intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+        //* Run something before a request is handled by the request handler
+        //* console.log('Running before the handler', context);
+        
         return handler.handle().pipe(
             map((data: any) => {
-                // Run something before the response is sent out
-                console.log('Running before response is sent out.', data);
+                //* Run something before the response is sent out
+                //* console.log('Running before response is sent out.', data);
+
+                //* conversion process
+                return plainToClass(this.dto, data, {
+                    excludeExtraneousValues: true
+                });
             })
         )
     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Get, Param, Post, Patch, Delete, Query, NotFoundException, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch, Delete, Query, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
     constructor(private userService: UsersService) {}
@@ -14,10 +17,11 @@ export class UsersController {
         this.userService.create(body.email, body.password);
     }
 
-    @UseInterceptors(SerializeInterceptor)
+    // @UseInterceptors(new SerializeInterceptor(UserDto))
+    
     @Get('/:id')
     async findUser(@Param('id') id: string){
-        console.log("handler is running.");
+        // console.log("handler is running.");
         const user = await this.userService.findOne(parseInt(id));
         if(!user) {
             throw new NotFoundException('User not found.');
