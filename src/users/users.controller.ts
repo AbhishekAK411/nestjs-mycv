ac4e05c9615ef22578e975b691bf077874e90475
Abhishek-kumar41 <abhishek.akumar411@gmail.com>
2024-07-25 16:07:17 +0530

    updates: current user, signout functionality implemented. changes to user service for findOne method.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,24 +13,38 @@ export class UsersController {
 
     constructor(private userService: UsersService, private authService: AuthService) {}
 
-    @Get('/colors/:color')
-    setColor(@Param('color') color: string, @Session() session: any) {
-        session.color = color;
+    // @Get('/colors/:color')
+    // setColor(@Param('color') color: string, @Session() session: any) {
+    //     session.color = color;
+    // }
+
+    // @Get('/colors')
+    // getColor(@Session() session: any) {
+    //     return session.color;
+    // }
+
+    @Get('/whoami')
+    whoAmI(@Session() session: any) {
+        return this.userService.findOne(session.userId);
     }
 
-    @Get('/colors')
-    getColor(@Session() session: any) {
-        return session.color;
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
     }
     
     @Post('signup')
-    signup(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async signup(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     @Post('signin')
-    signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     // @UseInterceptors(new SerializeInterceptor(UserDto))
