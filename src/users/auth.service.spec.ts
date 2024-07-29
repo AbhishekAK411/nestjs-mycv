9119383fdfa3f5c99706a03ac28ea7d4ac212534
Abhishek-kumar41 <abhishek.akumar411@gmail.com>
2024-07-29 13:10:10 +0530

    updates: auth service testing module implemented, with fake users service used for Testing DI Container

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,18 +1,22 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Test } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { UsersService } from "./users.service";
 
-describe('AuthService', () => {
-  let service: AuthService;
+it('Can create an instance of auth service.', async () => {
+  const fakeUserService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) => Promise.resolve({id: 1, email, password}) 
+  }
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService, {
+        provide: UsersService,
+        useValue: fakeUserService
+      }
+    ]
+  }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  const service = module.get(AuthService);
+  expect(service).toBeDefined();
+})
