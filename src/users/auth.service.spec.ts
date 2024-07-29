6e07d017e04ed61f83e7c8bd2138f8ca1cb014c8
Abhishek-kumar41 <abhishek.akumar411@gmail.com>
2024-07-29 16:11:45 +0530

    updates: 3 tests implemented for auth service, jest v26 is an older version where done and async calls can both work together, but in latest version, jest prohibits it.

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,22 +1,51 @@
-import { Test } from "@nestjs/testing";
-import { AuthService } from "./auth.service";
-import { UsersService } from "./users.service";
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
-it('Can create an instance of auth service.', async () => {
-  const fakeUserService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) => Promise.resolve({id: 1, email, password}) 
-  }
+describe('Auth Service', () => {
+  let service: AuthService;
+  let fakeUserService: Partial<UsersService>;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService, {
-        provide: UsersService,
-        useValue: fakeUserService
-      }
-    ]
-  }).compile();
+  beforeEach(async () => {
+    fakeUserService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
-})
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('Can create an instance of auth service.', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('Can create a new user with a salted and hashed password', async () => {
+    const user = await service.signup('asdf@asdf.com', 'asdf');
+
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('Throws an error if user signs up with email that is in use.', async () => {
+    fakeUserService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1'} as User]);
+    try {
+      await service.signup('asdf@asdf.com', 'asdf');
+    } catch (e) {
+      expect(e.toString()).toMatch('Email in use.');
+    }
+  });
+});
