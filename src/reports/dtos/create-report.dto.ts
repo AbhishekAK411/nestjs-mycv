e33503ec57901dfdbbec3c95e4f8e6fd405e8dab
Abhishek-kumar41 <abhishek.akumar411@gmail.com>
2024-08-04 10:38:27 +0530

    updates: report controller, service, entities, repository implemented

--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -1,3 +1,30 @@
+import { IsString, IsNumber, Min, Max, IsLongitude, IsLatitude, isLatitude } from "class-validator";
+
 export class CreateReportDto {
-    
+    @IsString()
+    make: string;
+
+    @IsString()
+    model: string;
+
+    @IsNumber()
+    @Min(1930)
+    @Max(2050)
+    year: number;
+
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
+    mileage: number;
+
+    @IsLatitude()
+    lat: number;
+
+    @IsLongitude()
+    lng: number;
+
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
+    price: number;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
     constructor(private repService: ReportsService) {}
 
     @Post()
+    @UseGuards(AuthGuard)
     createReport(@Body() body: CreateReportDto) {
-
+        return this.repService.create(body);
     }
 }
